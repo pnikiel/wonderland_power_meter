39feec19c4e11f4c5b55c9a4419830fe555b5952
Piotr Nikiel <piotr.nikiel@gmail.com>
2025-10-03 08:04:39 +0000

    reformat plus added a way to spec input from cmd line

--- power_meter.py
+++ power_meter.py
@@ -6,6 +6,7 @@ import sys
 
 import platform
 import paho.mqtt.client as paho
+import argparse
 
 def timestamp_of_pulse(gpio):
     gpio.wait_for_active()
@@ -45,34 +46,49 @@ def on_connect(client, userdata, flags, rc):
 def on_disconnect(client, userdata, rc):
     print('Disconnected')
 
-client_name = 'client-{0}-{1}'.format(node, sys.argv[0])
-print('Will connect as: {0}'.format(client_name))
+def main():
 
-cl = paho.Client(client_name)
-cl.on_connect = on_connect
-cl.on_disconnect = on_disconnect
-
-while True:
-    try:
-        cl.connect('wujskie-rpi-router')
-    except Exception as ex:
-        print ('Caught {0}, will retry.'.format(str(ex)))
-        continue
-    break
-
-cl.loop_start()
-
-b = Button(6)
-b.when_pressed = pulse_cbk
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_name', required=True)
 
 
-print('Waiting...')
-tlast = timestamp_of_pulse(b)
-while True:
-    time.sleep(10)
-    if current_power != None:
-        if current_power < 3600:
-            print(f'Publishing power as {current_power} ')
-            cl.publish('computing/nodes/{0}/power'.format(node), current_power)
-        else:
-            print(f'Filtered out bizarre reading of {current_power}, not published')
+    args = parser.parse_args()
+
+
+
+    client_name = 'client-{0}-{1}'.format(node, sys.argv[0])
+    print('Will connect as: {0}'.format(client_name))
+
+    cl = paho.Client(client_name)
+    cl.on_connect = on_connect
+    cl.on_disconnect = on_disconnect
+
+    while True:
+        try:
+            cl.connect('wujskie-rpi-router')
+        except Exception as ex:
+            print ('Caught {0}, will retry.'.format(str(ex)))
+            continue
+        break
+
+    cl.loop_start()
+
+    b = Button(6)
+    b.when_pressed = pulse_cbk
+
+
+    print('Waiting...')
+    tlast = timestamp_of_pulse(b)
+    while True:
+        time.sleep(10)
+        if current_power != None:
+            if current_power < 3600:
+                print(f'Publishing power as {current_power} ')
+                cl.publish('computing/nodes/{0}/power'.format(node), current_power)
+            else:
+                print(f'Filtered out bizarre reading of {current_power}, not published')
+
+
+if __name__ == "__main__":
+    main()
+
